25c46bcc44ffdfc3bdc6ab8538ab8b07e5d224aa
Sl-nethumax <maxnethu43@gmail.com>
2024-12-16 16:40:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '2NEQBJgR#wAhZrO3Foaybk8_dqHAUQmJHcPWpUTCcTNNpY4HP-hE', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || 'mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',    // Enter Your MongoDB URL
+    SESSION_ID: process.env.SESSION_ID || 'U9wwCLiZ#RGPEL89hclKMT81bAdAkCcvgbv4o94b0H9_Abmvizjs',
+    MONGODB: process.env.MONGODB || 'mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 };
