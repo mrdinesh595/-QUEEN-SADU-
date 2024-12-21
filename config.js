2a44f55c03d4f080975afaa0195f57239b81048e
mrdinesh595 <mrdineshofc@gmail.com>
2024-12-21 10:52:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'w0hCQDIb#10pkEkPh5N2lE3bRA-u6GSeKlwIU8IiP6Ez6H3Iikos',
+    SESSION_ID: process.env.SESSION_ID || 'w
+        Lv4FSBpR#nHGsn7qpkyVzS17WbLGOt_3UQkhflIMrAFcffdqdiMw',
     MONGODB: process.env.MONGODB || 'mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 };
