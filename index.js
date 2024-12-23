b5072f5613f4970e13369ac41aedf0fca40128bd
mrdinesh595 <mrdineshofc@gmail.com>
2024-12-23 11:36:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const axios = require('axios');
 const { File } = require('megajs');
 const moment = require('moment-timezone');
 
-const ownerNumber = ['94724534316'];
+const ownerNumber = ['94788001464];
 
 //------------------ Session ---------------------//
 
