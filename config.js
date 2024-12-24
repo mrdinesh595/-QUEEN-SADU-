8c03bade8aad261c526c78a79ea3eff43aab3b40
mrdinesh595 <mrdineshofc@gmail.com>
2024-12-24 15:12:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'XVh2WJpL#3rQrLEqyeBbs5kmDw7SEJA2bVPVrIEztyElJ9rnshUU',
+    SESSION_ID: process.env.SESSION_ID || 'add your session',
     MONGODB: process.env.MONGODB || 'mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     AUTO_VOICE:"true"
 };
