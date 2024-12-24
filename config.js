7d6daff4f399399005801c08ef7c9fd796e110b5
mrdinesh595 <mrdineshofc@gmail.com>
2024-12-24 15:28:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'add your session',
+    SESSION_ID: process.env.SESSION_ID || 'add your sesson id',
     MONGODB: process.env.MONGODB || 'mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    };
+};
